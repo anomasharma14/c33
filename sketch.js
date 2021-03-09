b90d05f3e7ca0823728bf75f77e0fdb933ca48a1
anomasharma <anomasharmadpsv@gmail.com>
2021-03-09 16:00:51 +0530

    added code

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ function draw(){
 
 function mouseDragged(){
     //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.setPosition(bird.body, {x:mouseX, y:mouseY});
     //}
 }
 
@@ -93,8 +93,11 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
+    if(keyCode === 32&& bird.body.speed<2){
+    
+     bird.trajectory=[];
+     Matter.Body.setPosition(bird.body, {x:200, y:50});
+    slingshot.attach(bird.body);
     }
 }
 
